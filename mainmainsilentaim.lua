f67df47ac0b90b24555b7c51703b964bac41d83f
ghoul <113273943+GhoulsLegit@users.noreply.github.com>
2022-11-17 18:45:07 -0500

    Create mainmainsilentaim.lua

---
+++ mainmainsilentaim.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GhoulsLegit/n/main/d"))()
+DaHoodSettings.Prediction = 0.1437
+Aiming.TargetPart = {"Head", "UpperTorso", "LowerTorso", "HumanoidRootPart", "RightFoot", "LeftFoot"}
+Aiming.FOV = 35
+Aiming.FOVSides = 25
+Aiming.HitChance = 400
+Aiming.ShowFOV = false
